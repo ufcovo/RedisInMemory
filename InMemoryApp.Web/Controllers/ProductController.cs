87bee1acab1119ff56f3813e46fc2a467ad2430e
Mert <79603812+ufcovo@users.noreply.github.com>
2023-06-25 21:23:18 +0300

    AbsoluteExpiration and SlidingExpiration

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,24 +14,34 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Index()
         {
             // First Way
-            if(String.IsNullOrEmpty(_memoryCache.Get<string>("time")))
-                _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            //if(String.IsNullOrEmpty(_memoryCache.Get<string>("time")))
+            //    _memoryCache.Set<string>("time", DateTime.Now.ToString());
 
             // Second Way
-            if(!_memoryCache.TryGetValue("time", out string timeCache))
-                _memoryCache.Set<string>("time", DateTime.Now.ToString());
-            
+            //if (!_memoryCache.TryGetValue("time", out string timeCache))
+            //{
+            //    _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
+            //}
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
             return View();
         }
 
         public IActionResult Show()
         {
-            _memoryCache.GetOrCreate<string>("time", entry =>
-            {
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("time", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
 
-            _memoryCache.Remove("time");
+            //_memoryCache.Remove("time");
+
+            _memoryCache.TryGetValue("time", out string timeCache);
+            ViewBag.timeForCache = timeCache;
+
             ViewBag.time = _memoryCache.Get<string>("time");
             return View();
         }
