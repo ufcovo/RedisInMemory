8edbd511fd547ad36792a4ad15136de940876cff
Mert <79603812+ufcovo@users.noreply.github.com>
2023-06-25 12:33:01 +0300

    Initial commit

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.time = _memoryCache.Get<string>("time");
+            return View();
+        }
+    }
+}
