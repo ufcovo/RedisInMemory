100c6f12b85206980b154ba980c8d9a5e5940b82
Mert <79603812+ufcovo@users.noreply.github.com>
2023-06-25 21:06:56 +0300

    TryGetValue, GetOrCreate and Remove Functions

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -13,12 +13,25 @@ namespace InMemoryApp.Web.Controllers
         }
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            // First Way
+            if(String.IsNullOrEmpty(_memoryCache.Get<string>("time")))
+                _memoryCache.Set<string>("time", DateTime.Now.ToString());
+
+            // Second Way
+            if(!_memoryCache.TryGetValue("time", out string timeCache))
+                _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            
             return View();
         }
 
         public IActionResult Show()
         {
+            _memoryCache.GetOrCreate<string>("time", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            _memoryCache.Remove("time");
             ViewBag.time = _memoryCache.Get<string>("time");
             return View();
         }
